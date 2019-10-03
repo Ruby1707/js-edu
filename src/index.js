c740af18413523db0d2239ba2ef16eeca44faebb
Ruby1707 <53220616+Ruby1707@users.noreply.github.com>
2019-10-03 17:47:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      if(knowsProgramming==true){
+        return Math.ceil(Number((800/config[focus])));
+      }
+      else{
+        return Math.ceil(Number((800/config[focus]))+Number((500/config[focus])));
+      }
   };
   
